5c9be41db2d7a5e2ada5b00d3cc1c50abba7d404
Jacob Parker <jacobparker1992@gmail.com>
2020-04-12 17:36:23 +0100

    Initial Commit

---
+++ ReasonML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReasonML
+//
+//  Created by Jacob Parker on 12/04/2020.
+//  Copyright © 2020 Jacob Parker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
